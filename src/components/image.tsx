8918d9159e2e837436254662396f1574bc4424a4
Yaroslav Dernov <yaroslav.dernov@russianpost.ru>
2025-08-04 21:37:35 +0400

    Updated Image component to exit when error image src is errored

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -1,4 +1,5 @@
-import type { JSX } from "react";
+import { cn } from "@/utils";
+import { useRef, useState, type JSX } from "react";
 
 type IntrinsicImgProps = JSX.IntrinsicElements["img"];
 
@@ -22,6 +23,13 @@ export const ImageComponent = ({
   errorImgSrc,
   className,
 }: Props) => {
+  const errorSrc = useRef<"provided" | "fallback" | "none" | "disabled">(
+    "none"
+  );
+  const [applyErrorClass, setApplyErrorClass] = useState(false);
+  const twClasses =
+    "after:content-['IMAGE_NOT_AVAILABLE'] after:flex after:items-center after:justify-center after:absolute after:inset-0 after:bg-gray-200 after:text-gray-700";
+  const fallbackErrorImgSrc = `https://placehold.co/${width || "400"}x${height || "400"}/CCCCCC/666666?text=Image+Error`;
   return (
     <img
       ref={(ref) => {
@@ -29,14 +37,27 @@ export const ImageComponent = ({
           ref?.style.setProperty("--img-url", `url(${previewSrc})`);
         }
       }}
-      className={`${previewSrc ? "[background-image:var(--img-url)]" : ""} bg-no-repeat bg-contain object-contain ${className}`}
+      className={cn(
+        previewSrc ? "[background-image:var(--img-url)]" : "",
+        "bg-no-repeat bg-contain object-contain overflow-clip relative",
+        applyErrorClass ? twClasses : "",
+        className
+      )}
       src={src}
       alt={alt}
       onError={(e) => {
-        if (errorImgSrc) {
-          e.currentTarget.src = errorImgSrc;
-        } else {
-          e.currentTarget.src = `https://placehold.co/${width || "400"}x${height || "400"}/CCCCCC/666666?text=Image+Error`;
+        if (!applyErrorClass) {
+          if (errorImgSrc && errorSrc.current === "none") {
+            e.currentTarget.src = errorImgSrc;
+            errorSrc.current = "provided";
+          } else if (errorSrc.current === "provided") {
+            e.currentTarget.src = fallbackErrorImgSrc;
+            errorSrc.current = "fallback";
+          } else if (errorSrc.current === "fallback") {
+            errorSrc.current = "disabled";
+            e.currentTarget.src = "";
+            setApplyErrorClass(true);
+          }
         }
       }}
       width={width}
--- src/type-utils.ts
+++ src/type-utils.ts
@@ -1,1 +1,5 @@
 export type PickKeys<T, K extends keyof T> = K;
+
+export type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
